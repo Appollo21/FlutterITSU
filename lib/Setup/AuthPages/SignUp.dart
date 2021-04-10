77d3d7a98d04e328377b2231593aa5be5d070733
Coffezka <42944711+Coffezka@users.noreply.github.com>
2021-04-10 15:35:50 +0300

    Верстка

--- lib/Setup/AuthPages/SignUp.dart
+++ lib/Setup/AuthPages/SignUp.dart
@@ -19,12 +19,15 @@ class _RegisterState extends State<Register>{
          Container(
           color: Color.fromRGBO(17,26,33,1),
           child: Form(
-          key: _formKey,
+              key: _formKey,
               child:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Container(
+                  margin: EdgeInsets.fromLTRB(33,0,0,0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Container(
                         child:Text(
@@ -58,54 +61,148 @@ class _RegisterState extends State<Register>{
                         Container(
                           margin: EdgeInsets.fromLTRB(17,17,17,0),
                           child:TextField(
-                            style: TextStyle(color: Colors.white),
+                            style: TextStyle(color: Color.fromRGBO(255,255,255,0.42)),
                             decoration: InputDecoration(
-                              focusColor: Color.fromRGBO(230,26,80,1),
-                              hintText: "Номер телефону або email"
+                              enabledBorder: UnderlineInputBorder(      
+                                borderSide: BorderSide(color: Color.fromRGBO(255,255,255,0.26)),   
+                              ),  
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Color.fromRGBO(230,26,80,1)),
+                              ),
+                              
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(255,255,255,0.42),
+                                fontSize:15,
+                                fontWeight: FontWeight.w400
+                              ),
+                              hintText: "Номер телефону або email" 
                             ),
                           )
                         ),
                         Container(
                           margin: EdgeInsets.fromLTRB(17,17,17,0),
                           child:TextField(
-                            style: TextStyle(color: Colors.white),
+                            style: TextStyle(
+                              color: Color.fromRGBO(255,255,255,0.42),
+                              fontSize:15,
+                              fontWeight: FontWeight.w400
+                              ),
+                            obscureText: true,
                             decoration: InputDecoration(
-                              focusColor: Color.fromRGBO(230,26,80,1),
-                              hintText: "Створіть пароль"
+                              enabledBorder: UnderlineInputBorder(      
+                                borderSide: BorderSide(color: Color.fromRGBO(255,255,255,0.26)),   
+                              ),  
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Color.fromRGBO(230,26,80,1)),
+                              ),
+                              
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(255,255,255,0.42)
+                              ),
+                              hintText: "Створіть пароль",
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.visibility_off_outlined,color: Color.fromRGBO(255,255,255,0.42)),
+                              ),
                             ),
                           )
                         ),
                         Container(
                           margin: EdgeInsets.fromLTRB(17,17,17,0),
                           child:TextField(
-                            style: TextStyle(color: Colors.white),
+                            style: TextStyle(color: Color.fromRGBO(255,255,255,0.42)),
+                            obscureText: true,
                             decoration: InputDecoration(
-                              focusColor: Color.fromRGBO(230,26,80,1),
-                              hintText: "Підтвердіть пароль"
+                              enabledBorder: UnderlineInputBorder(      
+                                borderSide: BorderSide(color: Color.fromRGBO(255,255,255,0.26)),   
+                              ),  
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Color.fromRGBO(230,26,80,1)),
+                              ),
+                              
+                              hintText: "Підтвердіть пароль",
+                              hintStyle: TextStyle(
+                                color: Color.fromRGBO(255,255,255,0.42),
+                                fontSize:15,
+                                fontWeight: FontWeight.w400
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.visibility_off_outlined,
+                                          color: Color.fromRGBO(255,255,255,0.42),),
+                              ),
                             ),
-                          )
+                          ),
                         ),
                         Container(
                           margin: EdgeInsets.fromLTRB(17,44,17,17),
                           child: FitnessButton("Далі",register),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              margin:EdgeInsets.fromLTRB(0,0,0,17),
+                              child: Text(
+                                "Вже заєстровані? ",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(255, 255, 255, 0.42),
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w500,
+                                  )
+                                
+                              ),
+                            ),
+                            Container(
+                              margin:EdgeInsets.fromLTRB(0,0,0,17),
+                              child: GestureDetector(
+                                onTap: () {},
+                                child:Text(
+                                  "Вxід!",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(230,26,80,1),
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                    )
+                                )
+                              )
+                            )
+                          ]
                         ),
                       ]
                   )
                 ),
                 ),
 
-                Container(
-                    child: Text(
-                      "Ознайомитися з привилами користування можна Тут!",
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.42),
-                        fontSize: 10,
-                        fontWeight: FontWeight.w500,
-                        )
-                      
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        "Ознайомитися з привилами користування можна ",
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.42),
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          )
+                        
+                      ),
                     ),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          
+                        }, 
+                        child: Text(
+                          "Тут!",
+                          style: TextStyle(
+                            color: Color.fromRGBO(230,26,80,1),
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            )
+                        )
+                      ),
+                    )
+                  ]
                 )
-
               ],
               
             ),
